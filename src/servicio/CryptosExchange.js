aea66e2bd30c1cc8be68e6524b1cd616564250af
MartinMedinaRuvian <martinjesusmr@ufps.edu.co>
2022-05-18 22:21:31 -0500

    Ajuste en un solo endpoint

--- src/servicio/CryptosExchange.js
+++ src/servicio/CryptosExchange.js
@@ -3,16 +3,34 @@ const endpoints = express.Router()
 const axios = require('axios')
 const config = require('../config/config')
 
-// Imports the Google Cloud client library
 const {PubSub} = require('@google-cloud/pubsub');
 
-// Creates a client; cache this for further use
 const pubSubClient = new PubSub();
 GOOGLE_APPLICATION_CREDENTIALS = '.\cryptobot-345516-0047de703f40.json'
 
-endpoints.post('/precios/:cantidad_cryptos/:tipo_moneda', async(req, res)=> {
-  let { tipo_moneda } = req.params
-  let { cantidad_cryptos } = req.params
+endpoints.post('/exchange/:tipo_solicitud', async(req, res) => {
+  const { tipo_solicitud } = req.params
+  const { tipo_moneda, cantidad_cryptos, nombre } = req.body
+  let datos = 'No se encontro el tipo de solicitud ' + tipo_solicitud
+  if (tipo_solicitud == 'precios') {
+    const cantidadCryptos = (!cantidad_cryptos || cantidad_cryptos == '' || cantidad_cryptos == undefined) ? 5 : cantidad_cryptos
+    const tipoMoneda = (!tipo_moneda || tipo_moneda == '' || tipo_moneda == undefined) ? 'USD' : tipo_moneda
+    datos = await consultarPreciosPorCantidad(cantidadCryptos, tipoMoneda)
+  } else if (tipo_solicitud == 'precio') {
+    const nombreCrypto = (!nombre || nombre == '' || nombre == undefined) ? 'Bitcoin' : capitalizarPrimeraLetra(nombre)
+    const tipoMoneda = (!tipo_moneda || tipo_moneda == '' || tipo_moneda == undefined) ? 'USD' : tipo_moneda
+    datos = await consultarPrecioPorNombre(nombreCrypto, tipoMoneda)
+  }
+  res.json(datos)
+  datos = JSON.stringify(datos)
+  publishMessage(datos)
+})
+
+function capitalizarPrimeraLetra(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+async function consultarPreciosPorCantidad (cantidad_cryptos, tipo_moneda) {
   let precioDolarColombia = 0
   const formatterUSD = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -43,62 +61,53 @@ endpoints.post('/precios/:cantidad_cryptos/:tipo_moneda', async(req, res)=> {
         precios.push(precioCrypto)
       }
     }
-    res.json(precios)
-    precios = JSON.stringify(precios)
-    publishMessage(precios)
+    return precios
   } else {
-    res.json({
-      'Mensaje':'No se encontraron datos.'
-    })   
+    return 'No se encontraron los precios'
   }
-})
+}
 
-endpoints.post('/precio/:nombre/:tipo_moneda', async(req, res)=> {
-    let { nombre, tipo_moneda } = req.params
-    let precioCrypto =  {
-      nombre: '',
-      valor: 0
+async function consultarPrecioPorNombre (nombre, tipo_moneda) {
+  let precioCrypto =  {
+    nombre: '',
+    valor: 0
+  }
+  let precioDolarColombia = 0
+  const formatterUSD = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0
+  })
+  const formatterPeso = new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 0
+  })
+  try {
+    if (tipo_moneda === 'COP') {
+      let precioDolarConsultadoAPI = await consultarPrecioDolarColombia()
+      precioDolarColombia = (precioDolarConsultadoAPI > 0) ? precioDolarConsultadoAPI : 3800
     }
-    let precioDolarColombia = 0
-    const formatterUSD = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 0
-    })
-    const formatterPeso = new Intl.NumberFormat('es-CO', {
-      style: 'currency',
-      currency: 'COP',
-      minimumFractionDigits: 0
-    })
-    try {
-      if (tipo_moneda === 'COP') {
-        let precioDolarConsultadoAPI = await consultarPrecioDolarColombia()
-        precioDolarColombia = (precioDolarConsultadoAPI > 0) ? precioDolarConsultadoAPI : 3800
-      }
-      let responsePrecios = await axios.get(config.api_exchange)
-      if (responsePrecios && responsePrecios.data.length > 0) {
-        for (let precio of responsePrecios.data) {
-          if (precio.name === nombre) {
-            let valor = (tipo_moneda === 'USD') ? precio.current_price : (precio.current_price * precioDolarColombia)
-            precioCrypto.nombre = precio.name
-            precioCrypto.valor = (tipo_moneda === 'USD') ? formatterUSD.format(valor) : formatterPeso.format(valor)
-            break   
-          }
+    let responsePrecios = await axios.get(config.api_exchange)
+    if (responsePrecios && responsePrecios.data.length > 0) {
+      for (let precio of responsePrecios.data) {
+        if (precio.name === nombre) {
+          let valor = (tipo_moneda === 'USD') ? precio.current_price : (precio.current_price * precioDolarColombia)
+          precioCrypto.nombre = precio.name
+          precioCrypto.valor = (tipo_moneda === 'USD') ? formatterUSD.format(valor) : formatterPeso.format(valor)
+          break   
         }
       }
-    } catch (error) {
-      console.log(error)
     }
-    if (precioCrypto.nombre !== '') {
-      res.json(precioCrypto)
-      precioCrypto = JSON.stringify(precioCrypto)
-      publishMessage(precioCrypto)
-    } else {
-      res.json({
-        'Mensaje': 'No se ha encontrado la crypto moneda con el nombre ' + nombre
-      })
-    }
-})
+  } catch (error) {
+    console.log(error)
+  }
+  if (precioCrypto.nombre !== '') {
+    return precioCrypto
+  } else {
+    return 'No se encontro el precio de la moneda con el nombre ' + nombre
+  }
+}
 
 let consultarPrecioDolarColombia = async () => {
   let date = new Date()
@@ -127,10 +136,5 @@ async function publishMessage(messaging) {
     process.exitCode = 1;
   }
 }
-
-/**
- * Conectar a google cloud login
- * gcloud auth application-default login
- */
 
 module.exports = endpoints
