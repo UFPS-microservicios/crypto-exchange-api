93809b032092b634be6b0fce9b389b5ecb4aa6a9
MartinMedinaRuvian <martinjesusmr@ufps.edu.co>
2022-05-18 21:14:28 -0500

    Ajuste enpoint y pubsub

--- src/servicio/CryptosExchange.js
+++ src/servicio/CryptosExchange.js
@@ -3,7 +3,14 @@ const endpoints = express.Router()
 const axios = require('axios')
 const config = require('../config/config')
 
-endpoints.get('/precios/:cantidad_cryptos/:tipo_moneda', async(req, res)=> {
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub();
+GOOGLE_APPLICATION_CREDENTIALS = '.\cryptobot-345516-0047de703f40.json'
+
+endpoints.post('/precios/:cantidad_cryptos/:tipo_moneda', async(req, res)=> {
   let { tipo_moneda } = req.params
   let { cantidad_cryptos } = req.params
   let precioDolarColombia = 0
@@ -37,26 +44,44 @@ endpoints.get('/precios/:cantidad_cryptos/:tipo_moneda', async(req, res)=> {
       }
     }
     res.json(precios)
+    precios = JSON.stringify(precios)
+    publishMessage(precios)
   } else {
     res.json({
       'Mensaje':'No se encontraron datos.'
-    })
+    })   
   }
 })
 
-endpoints.get('/precio/:nombre', async(req, res)=> {
-    let { nombre } = req.params
+endpoints.post('/precio/:nombre/:tipo_moneda', async(req, res)=> {
+    let { nombre, tipo_moneda } = req.params
     let precioCrypto =  {
       nombre: '',
       valor: 0
     }
+    let precioDolarColombia = 0
+    const formatterUSD = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0
+    })
+    const formatterPeso = new Intl.NumberFormat('es-CO', {
+      style: 'currency',
+      currency: 'COP',
+      minimumFractionDigits: 0
+    })
     try {
+      if (tipo_moneda === 'COP') {
+        let precioDolarConsultadoAPI = await consultarPrecioDolarColombia()
+        precioDolarColombia = (precioDolarConsultadoAPI > 0) ? precioDolarConsultadoAPI : 3800
+      }
       let responsePrecios = await axios.get(config.api_exchange)
       if (responsePrecios && responsePrecios.data.length > 0) {
         for (let precio of responsePrecios.data) {
           if (precio.name === nombre) {
+            let valor = (tipo_moneda === 'USD') ? precio.current_price : (precio.current_price * precioDolarColombia)
             precioCrypto.nombre = precio.name
-            precioCrypto.valor = precio.current_price
+            precioCrypto.valor = (tipo_moneda === 'USD') ? formatterUSD.format(valor) : formatterPeso.format(valor)
             break   
           }
         }
@@ -66,6 +91,8 @@ endpoints.get('/precio/:nombre', async(req, res)=> {
     }
     if (precioCrypto.nombre !== '') {
       res.json(precioCrypto)
+      precioCrypto = JSON.stringify(precioCrypto)
+      publishMessage(precioCrypto)
     } else {
       res.json({
         'Mensaje': 'No se ha encontrado la crypto moneda con el nombre ' + nombre
@@ -87,5 +114,23 @@ let consultarPrecioDolarColombia = async () => {
   }
   return 0
 }
+
+async function publishMessage(messaging) {
+  const dataBuffer = Buffer.from(messaging);
+  try {
+    const messageId = await pubSubClient
+      .topic("projects/cryptobot-345516/topics/value-topic")
+      .publishMessage({data:dataBuffer});
+    console.log(`Message ${messageId} published.`);
+  } catch (error) {
+    console.error(`Received error while publishing: ${error.message}`);
+    process.exitCode = 1;
+  }
+}
+
+/**
+ * Conectar a google cloud login
+ * gcloud auth application-default login
+ */
 
 module.exports = endpoints
