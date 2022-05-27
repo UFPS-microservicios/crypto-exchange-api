477b4fc6a2c209684fe6ef0df98ddb651d1c2562
MartinMedinaRuvian <martinjesusmr@ufps.edu.co>
2022-05-27 08:32:16 -0500

    Prueba

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,7 @@ const apis = {
     api_exchange: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false',
     api_exchange_history: 'https://api.coingecko.com/api/v3/coins/bitcoin/history?date=19-03-2022&localization=EN',
     api_dolar_colombia: 'https://www.datos.gov.co/resource/32sa-8pi3.json'
+    //Prueba
 }
 
 module.exports = apis
